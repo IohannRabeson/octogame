713ffa92e9ca353cb4efa232045d7b902d17a884
irabeson <irabeson@student.42.fr>
2015-06-17 14:50:48 +0200

    Cppcheck now checks only game sources

--- scripts/linux/run_static_analysis.sh
+++ scripts/linux/run_static_analysis.sh
@@ -2,8 +2,4 @@
 
 DIRS="Managers Firefly Octo Map Physics Decors Main Managers"
 
-cppcheck --inline-suppr --quiet --enable=all --error-exitcode=1 --std=c++11 --force -q --platform=unix64 -I $DIRS -I /usr/local/include/ -I ./octolib/includes $DIRS 2>&1 | grep -e "warning" -e "error" > cppcheck_results.txt
-if [ -s cppcheck_results.txt ]; then
-	cat cppcheck_results.txt
-	exit 1
-fi
+cppcheck --quiet --inline-suppr --enable=warning --error-exitcode=1 --std=c++11 --force --platform=unix64 -I $DIRS -I/usr/local/include/ $DIRS
