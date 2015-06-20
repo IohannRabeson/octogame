7fde687b29cbc450b0d1631beefe7db677e8b288
fcorbel <fcorbel@student.42.fr>
2015-06-20 16:45:12 +0200

    Use of uniform_real_distribution instead of uniform_int_distribution

--- Decors/Tree.cpp
+++ Decors/Tree.cpp
@@ -179,7 +179,7 @@ float Tree::randomFloat(float min, float max)
 {
 	if (max - min == 0)
 		return max;
-	std::uniform_int_distribution<float> distribution(min, max);
+	std::uniform_real_distribution<float> distribution(min, max);
 	std::random_device rd;
 	std::mt19937 engine(rd());
 
