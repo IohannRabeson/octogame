62040c4b56d4e800d979dc408be8e421cfd8b780
fcorbel <fcorbel@student.42.fr>
2015-06-23 12:03:23 +0200

    Use of uniform_real_distribution instead of uniform_int_distribution

--- Decors/Crystal.cpp
+++ Decors/Crystal.cpp
@@ -141,7 +141,7 @@ float Crystal::randomFloat(float min, float max)
 {
 	if (max - min == 0)
 		return max;
-	std::uniform_int_distribution<float> distribution(min, max);
+	std::uniform_real_distribution<float> distribution(min, max);
 	std::random_device rd;
 	std::mt19937 engine(rd());
 
