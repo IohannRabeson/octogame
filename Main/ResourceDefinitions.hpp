cba01888f4642634563fc1cafd394c7a9f454191
fcorbel <fcorbel@student.42.fr>
2015-08-07 20:23:16 +0200

    Push ResourceDefinitions Delete some old test

---
+++ Main/ResourceDefinitions.hpp
@@ -0,0 +1,13 @@
+//
+//	/ResourceDefinitions.hpp
+#if !defined OCTO_MAIN_RESOURCEDEFINITIONS_HPP
+#define OCTO_MAIN_RESOURCEDEFINITIONS_HPP
+#include <cstdint>
+
+#include "ToClassifyDefinitions.hpp"
+#include "SoundsDefinitions.hpp"
+#include "ImagesDefinitions.hpp"
+#include "ColorsDefinitions.hpp"
+#include "LoadingDefinitions.hpp"
+
+#endif // OCTO_MAIN_RESOURCEDEFINITIONS_HPP
