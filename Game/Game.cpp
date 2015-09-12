f3a3324169c30814ac6bdf7ced723824a6fe44e1
jbalestr42 <julien.balestrieri@gmail.com>
2015-09-12 17:59:43 +0200

    add collision box

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -135,6 +135,7 @@ void	Game::draw(sf::RenderTarget& render, sf::RenderStates states)const
 	render.draw(m_skyManager->getDecorsBack(), states);
 	render.draw(*m_parallaxScrolling, states);
 	m_groundManager->drawBack(render, states);
+	m_physicsEngine.debugDraw(render);
 	render.draw(*m_octo, states);
 	m_groundManager->drawFront(render, states);
 	render.draw(m_skyManager->getDecorsFront(), states);
--- Game/NanoRobot.hpp
+++ Game/NanoRobot.hpp
@@ -8,11 +8,11 @@
 # include "FireflyPopulation.hpp"
 # include "IPlaceable.hpp"
 
-//TODO NanoRobo n'héritera pas de game Object, mais les sous classes en hériteront
-class NanoRobot : public AGameObject<GameObjectType::NanoRobot>, public IPlaceable
+class CircleShape;
+
+class NanoRobot : public IPlaceable
 {
 public:
-	NanoRobot(sf::Vector2f const & position);
 	virtual ~NanoRobot(void);
 
 	void setPosition(sf::Vector2f const & position);
@@ -20,8 +20,11 @@ public:
 	void update(sf::Time frameTime);
 	void draw(sf::RenderTarget& render, sf::RenderStates states) const;
 
+protected:
+	NanoRobot(sf::Vector2f const & position, std::string id, std::size_t nbFrames);
+	void setup(AGameObjectBase * gameObject);
+
 private:
-	// TODO: add CircleShape
 	// TODO: add Bubble
 	FireflySwarm					m_swarm;
 	FireflySwarm::UniformPopulation	m_uniformPopulation;
@@ -29,6 +32,8 @@ private:
 
 	octo::AnimatedSprite			m_sprite;
 	octo::SpriteAnimation			m_animation;
+
+	CircleShape *					m_box;
 
 };
 
