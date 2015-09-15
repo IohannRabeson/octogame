f9ee3749b63fbb4de1c1124a863a9082b250e3eb
glasset <glasset@github.com>
2015-09-15 11:47:08 +0200

    add test idle/death

--- Game/OctoSound.cpp
+++ Game/OctoSound.cpp
@@ -1,16 +1,26 @@
 #include "OctoSound.hpp"
-#include "AudioManager.hpp"
+#include "ResourceDefinitions.hpp"
+#include <Application.hpp>
+#include <ResourceManager.hpp>
 
-CharacterOcto::OctoSound::OctoSound()
+CharacterOcto::OctoSound::OctoSound() :
+	m_sound(nullptr)
 {
 }
 
 void	CharacterOcto::OctoSound::update(sf::Time frameTime, Events event)
 {
-	(void)frameTime;
+	octo::AudioManager&		audio = octo::Application::getAudioManager();
+	octo::ResourceManager&	resources = octo::Application::getResourceManager();
+
+	if (m_prevEvent == event)
+		return;
+	if (m_sound != nullptr)
+		m_sound->stop();
 	switch(event)
 	{
 		case Idle:
+			m_sound = audio.playSound(resources.getSound(OCTO_MONOLOGUE_WAV));
 			break;
 		case Right:
 		case Left:
@@ -19,7 +29,11 @@ void	CharacterOcto::OctoSound::update(sf::Time frameTime, Events event)
 			break;
 		case DoubleJump:
 			break;
+		case Death:
+			m_sound = audio.playSound(resources.getSound(OCTO_DEATH_WAV));
 		default:
 			break;
 	}
+	m_prevEvent = event;
+	(void)frameTime;
 }
--- Game/OctoSound.hpp
+++ Game/OctoSound.hpp
@@ -1,6 +1,7 @@
 #ifndef OCTOSOUND_HPP
 # define OCTOSOUND_HPP
 # include "CharacterOcto.hpp"
+# include <AudioManager.hpp>
 
 class CharacterOcto::OctoSound
 {
@@ -8,6 +9,8 @@ public:
 	OctoSound();
 	void	update(sf::Time frameTime, Events event);
 private:
+	std::shared_ptr<sf::Sound>	m_sound;
+	Events						m_prevEvent;
 };
 
 #endif
