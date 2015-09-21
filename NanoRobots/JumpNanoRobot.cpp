4e307b2cf327269f314286909e9dd2c53fea4d9b
fcorbel <fcorbel@student.42.fr>
2015-09-21 13:42:14 +0200

    Replace nano robot jump

--- NanoRobots/JumpNanoRobot.cpp
+++ NanoRobots/JumpNanoRobot.cpp
@@ -2,7 +2,7 @@
 #include "ResourceDefinitions.hpp"
 
 JumpNanoRobot::JumpNanoRobot(void) :
-	NanoRobot(sf::Vector2f(9800, -1400.f), NANO_JUMP_OSS, 4, 654)
+	NanoRobot(sf::Vector2f(9700, -1400.f), NANO_JUMP_OSS, 4, 654)
 {
 	setup(this);
 }
