e22b127df2058a16351161ad868f6b4d976e52cf
jbalestr42 <julien.balestrieri@gmail.com>
2015-09-22 17:31:20 +0200

    improve animation

--- Biomes/LevelTwoBiome.cpp
+++ Biomes/LevelTwoBiome.cpp
@@ -135,7 +135,7 @@ LevelTwoBiome::LevelTwoBiome() :
 	m_gameObjects[375] = GameObjectType::TurbanNpc;
 	m_gameObjects[592] = GameObjectType::FannyNpc;
 	m_gameObjects[630] = GameObjectType::RepairNanoRobot;
-	m_gameObjects[658] = GameObjectType::Bouibouik;
+	m_gameObjects[645] = GameObjectType::Bouibouik;
 	m_gameObjects[720] = GameObjectType::OldDesertStaticNpc;
 	m_gameObjects[680] = GameObjectType::Tent;
 	m_gameObjects[740] = GameObjectType::Portal;
--- GameObjects/Bouibouik.cpp
+++ GameObjects/Bouibouik.cpp
@@ -1,22 +1,33 @@
 #include "Bouibouik.hpp"
 #include <Interpolations.hpp>
+#include <Application.hpp>
+#include <ResourceManager.hpp>
 
 Bouibouik::Bouibouik(void) :
-	SimpleObject(BOUIBOUIK_OSS, PERSISTENCE_FRAG, 20.f)
+	SimpleObject(BOUIBOUIK_BACK_OSS, PERSISTENCE_FRAG, 20.f)
 {
 	typedef octo::SpriteAnimation::Frame	Frame;
-	setupAnimation({Frame(sf::seconds(0.2f), 0u),
-					Frame(sf::seconds(0.2f), 1u),
-					Frame(sf::seconds(0.2f), 2u),
-					Frame(sf::seconds(0.2f), 1u)}, octo::LoopMode::Loop);
+	setupAnimation({Frame(sf::seconds(0.2f), 0u)}, octo::LoopMode::Loop);
 	setupSmoke();
+
+	octo::ResourceManager & resources = octo::Application::getResourceManager();
+
+	typedef octo::SpriteAnimation::Frame	Frame;
+	m_animationFront.setFrames({Frame(sf::seconds(0.2f), 0u),
+								Frame(sf::seconds(0.2f), 1u),
+								Frame(sf::seconds(0.2f), 2u),
+								Frame(sf::seconds(0.2f), 1u)});
+	m_animationFront.setLoop(octo::LoopMode::Loop);
+	m_spriteFront.setSpriteSheet(resources.getSpriteSheet(BOUIBOUIK_FRONT_OSS));
+	m_spriteFront.setScale(1.f, 1.f);
+	m_spriteFront.setAnimation(m_animationFront);
+	m_spriteFront.play();
 
 	setupBox(this, static_cast<std::size_t>(GameObjectType::Bouibouik), static_cast<std::size_t>(GameObjectType::Player));
 }
 
 Bouibouik::~Bouibouik(void)
-{
-}
+{}
 
 void Bouibouik::setupSmoke(void)
 {
@@ -28,6 +39,15 @@ void Bouibouik::setupSmoke(void)
 	m_smoke.setScaleFactor(10.f);
 	m_smoke.setDispersion(80.f);
 	m_smoke.setColor(sf::Color(18, 14, 66, 150));
+
+	m_smoke2.setup(sf::Vector2f(5.f, 5.f));
+	m_smoke2.setVelocity(sf::Vector2f(0.f, -100.f));
+	m_smoke2.setEmitTimeRange(0.2f, 0.3f);
+	m_smoke2.setGrowTimeRange(0.4f, 0.6f);
+	m_smoke2.setLifeTimeRange(0.6f, 0.8f);
+	m_smoke2.setScaleFactor(10.f);
+	m_smoke2.setDispersion(80.f);
+	m_smoke2.setColor(sf::Color(18, 14, 66, 150));
 }
 
 void Bouibouik::startBalle(void)
@@ -37,17 +57,24 @@ void Bouibouik::startBalle(void)
 	m_smoke.setEmitTimeRange(0.05f, 0.1f);
 	m_smoke.setScaleFactor(15.f);
 	m_smoke.setDispersion(150.f);
+	m_smoke2.setVelocity(sf::Vector2f(0.f, -200.f));
+	m_smoke2.setEmitTimeRange(0.05f, 0.1f);
+	m_smoke2.setScaleFactor(15.f);
+	m_smoke2.setDispersion(150.f);
 }
 
 void Bouibouik::setPosition(sf::Vector2f const & position)
 {
 	SimpleObject::setPosition(position - sf::Vector2f(0.f, 370.f));
-	m_smoke.setPosition(position - sf::Vector2f(-55.f, 350.f));
+	m_spriteFront.setPosition(position - sf::Vector2f(0.f, 370.f));
+	m_smoke.setPosition(position + sf::Vector2f(315.f, -350.f));
+	m_smoke2.setPosition(position + sf::Vector2f(235.f, -325.f));
 }
 
 void Bouibouik::update(sf::Time frameTime)
 {
 	m_smoke.update(frameTime);
+	m_smoke2.update(frameTime);
 	sf::Shader & shader = getShader();
 
 	if (getStartBalle())
@@ -70,12 +97,23 @@ void Bouibouik::update(sf::Time frameTime)
 		m_smoke.setEmitTimeRange(0.2f, 0.3f);
 		m_smoke.setScaleFactor(10.f);
 		m_smoke.setDispersion(80.f);
+		m_smoke2.setVelocity(sf::Vector2f(0.f, -100.f));
+		m_smoke2.setEmitTimeRange(0.2f, 0.3f);
+		m_smoke2.setScaleFactor(10.f);
+		m_smoke2.setDispersion(80.f);
 	}
 	SimpleObject::update(frameTime);
+	m_spriteFront.update(frameTime);
 }
 
 void Bouibouik::draw(sf::RenderTarget& render, sf::RenderStates states) const
 {
 	SimpleObject::draw(render, states);
+}
+
+void Bouibouik::drawFront(sf::RenderTarget& render, sf::RenderStates states) const
+{
+	render.draw(m_spriteFront, states);
 	m_smoke.draw(render);
+	m_smoke2.draw(render);
 }
