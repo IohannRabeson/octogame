0e22c31c40563f49f5765df2dd0cc93e4c423fd8
fcorbel <fcorbel@student.42.fr>
2015-09-26 17:49:31 +0200

    Modify map

--- NanoRobots/DoubleJumpNanoRobot.cpp
+++ NanoRobots/DoubleJumpNanoRobot.cpp
@@ -2,7 +2,7 @@
 #include "ResourceDefinitions.hpp"
 
 DoubleJumpNanoRobot::DoubleJumpNanoRobot(void) :
-	NanoRobot(sf::Vector2f(200.f, 800.f), NANO_DOUBLE_JUMP_OSS, 3, 18524)
+	NanoRobot(sf::Vector2f(8236.f, -2200.f), NANO_DOUBLE_JUMP_OSS, 3, 18524)
 {
 	setup(this);
 }
