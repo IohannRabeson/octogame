1e5167590059a7cd2e20d430e41708e5b927e109
fcorbel <fcorbel@student.42.fr>
2015-09-26 17:57:24 +0200

    Resolve conflict

--- NanoRobots/DoubleJumpNanoRobot.cpp
+++ NanoRobots/DoubleJumpNanoRobot.cpp
@@ -2,11 +2,7 @@
 #include "ResourceDefinitions.hpp"
 
 DoubleJumpNanoRobot::DoubleJumpNanoRobot(void) :
-<<<<<<< HEAD
-	NanoRobot(sf::Vector2f(8236.f, -2200.f), NANO_DOUBLE_JUMP_OSS, 3, 18524)
-=======
-	NanoRobot(sf::Vector2f(200.f, 800.f), NANO_DOUBLE_JUMP_OSS, 3, 18524, sf::Vector2f(0.f, -22.f))
->>>>>>> dev
+	NanoRobot(sf::Vector2f(8236.f, -2200.f), NANO_DOUBLE_JUMP_OSS, 3, 18524, sf::Vector2f(0.f, -22.f))
 {
 	setup(this);
 
