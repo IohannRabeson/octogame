170c59a25985e1fbf53a80cb088051c8eb1cdba8
fcorbel <fcorbel@student.42.fr>
2015-09-28 02:51:31 +0200

    Add birds in jungle

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -332,12 +332,13 @@ void	Game::draw(sf::RenderTarget& render, sf::RenderStates states)const
 	render.clear();
 	render.draw(m_skyManager->getDecorsBack(), states);
 	render.draw(*m_parallaxScrolling, states);
-	m_physicsEngine.debugDraw(render);
+	//m_physicsEngine.debugDraw(render);
 	m_groundManager->drawBack(render, states);
 	render.draw(*m_octo, states);
 	m_groundManager->drawFront(render, states);
 	render.draw(m_skyManager->getDecorsFront(), states);
 	m_octo->drawNanoRobot(render, states);
+	m_skyManager->drawBirds(render, states);
 	m_groundManager->drawWater(render, states);
 	render.draw(m_skyManager->getFilter(), states);
 	m_groundManager->drawText(render, states);
