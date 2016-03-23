2b73fb9ba1e17a81fa657fa9d3c7bddb550f1882
fcorbel <fcorbel@student.42.fr>
2016-03-23 16:08:36 +0100

    Working controller (with bug)

--- Game/InputListener.cpp
+++ Game/InputListener.cpp
@@ -2,6 +2,8 @@
 #include <GraphicsManager.hpp>
 #include "InputListener.hpp"
 
+#include <iostream>
+
 InputListener::InputListener(void)
 {
 }
@@ -24,12 +26,14 @@ void	InputListener::addInputListener(void)
 {
 	octo::GraphicsManager & graphics = octo::Application::getGraphicsManager();
 	graphics.addKeyboardListener(this);
+	graphics.addJoystickListener(this);
 }
 
 void	InputListener::removeInputListener(void)
 {
 	octo::GraphicsManager & graphics = octo::Application::getGraphicsManager();
 	graphics.removeKeyboardListener(this);
+	graphics.removeJoystickListener(this);
 }
 
 bool	InputListener::onPressed(sf::Event::KeyEvent const& event)
@@ -115,24 +119,117 @@ bool	InputListener::onReleased(sf::Event::KeyEvent const& event)
 
 void	InputListener::onMoved(sf::Event::JoystickMoveEvent const& event)
 {
-	(void)event;
-	if (sf::Joystick::isConnected(0))
+	if (sf::Joystick::isConnected(0) && event.joystickId == 0)
 	{
+		if (event.axis == sf::Joystick::X)
+		{
+			if (event.position > 50)
+				onInputPressed(OctoKeys::Right);
+			else if (event.position < -50)
+				onInputPressed(OctoKeys::Left);
+			else
+			{
+				onInputReleased(OctoKeys::Right);
+				onInputReleased(OctoKeys::Left);
+			}
+		}
+
+		if (event.axis == sf::Joystick::Y)
+		{
+			if (event.position < -50)
+				onInputPressed(OctoKeys::Up);
+			else if (event.position > 50)
+				onInputPressed(OctoKeys::Down);
+			else
+			{
+				onInputReleased(OctoKeys::Up);
+				onInputReleased(OctoKeys::Down);
+			}
+		}
 	}
 }
+
 void	InputListener::onPressed(sf::Event::JoystickButtonEvent const& event)
 {
-	(void)event;
-	if (sf::Joystick::isConnected(0))
+	if (sf::Joystick::isConnected(0) && event.joystickId == 0)
 	{
+		switch (event.button)
+		{
+			case 7:
+				onInputPressed(OctoKeys::Left);
+				break;
+			case 5:
+				onInputPressed(OctoKeys::Right);
+				break;
+			case 14:
+				onInputPressed(OctoKeys::Space);
+				break;
+			case 4:
+				onInputPressed(OctoKeys::Up);
+				break;
+			case 6:
+				onInputPressed(OctoKeys::Down);
+				break;
+			case 13:
+				onInputPressed(OctoKeys::Use);
+				break;
+			case 9:
+				onInputPressed(OctoKeys::GroundRight);
+				break;
+			case 8:
+				onInputPressed(OctoKeys::GroundLeft);
+				break;
+//			case 15:
+//				onInputPressed(OctoKeys::Return);
+//				break;
+			case 3:
+				onInputPressed(OctoKeys::Escape);
+				break;
+			default:
+				break;
+		}
 	}
 }
 
 void	InputListener::onReleased(sf::Event::JoystickButtonEvent const& event)
 {
-	(void)event;
-	if (sf::Joystick::isConnected(0))
+	if (sf::Joystick::isConnected(0) && event.joystickId == 0)
 	{
+		switch (event.button)
+		{
+			case 7:
+				onInputReleased(OctoKeys::Left);
+				break;
+			case 5:
+				onInputReleased(OctoKeys::Right);
+				break;
+			case 14:
+				onInputReleased(OctoKeys::Space);
+				break;
+			case 4:
+				onInputReleased(OctoKeys::Up);
+				break;
+			case 6:
+				onInputReleased(OctoKeys::Down);
+				break;
+			case 13:
+				onInputReleased(OctoKeys::Use);
+				break;
+			case 9:
+				onInputReleased(OctoKeys::GroundRight);
+				break;
+			case 8:
+				onInputReleased(OctoKeys::GroundLeft);
+				break;
+//			case 15:
+//				onInputReleased(OctoKeys::Return);
+//				break;
+			case 3:
+				onInputReleased(OctoKeys::Escape);
+				break;
+			default:
+				break;
+		}
 	}
 }
 
