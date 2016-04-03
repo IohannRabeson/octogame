2adad58bf6a37f3572a1655062acc0214b2412f3
fcorbel <fcorbel@student.42.fr>
2016-04-03 19:50:15 +0200

    New camera system

--- Game/CameraMovement.hpp
+++ Game/CameraMovement.hpp
@@ -4,6 +4,8 @@
 # include <SFML/System/Vector2.hpp>
 # include <SFML/System/Time.hpp>
 
+# include <iostream>
+
 class CameraMovement
 {
 public:
@@ -14,18 +16,24 @@ public:
 	void collideZoom(sf::Time frametime);
 	void collideZoomEvent(sf::Vector2f const & npcPos);
 	void setEventFallTimer(sf::Time const & eventFallTimer);
+	void setFall(bool isFall);
 
 private:
 	sf::Vector2f								m_initialSize;
 	sf::Vector2f								m_currentSize;
 	float										m_zoomFactor;
 	bool										m_isZoom;
+	bool										m_isFall;
 	sf::Time									m_eventFallTimer;
 	sf::Vector2f								m_npcPos;
 	sf::Time									m_timerZoom;
 	sf::Time									m_timerZoomMax;
 	sf::Time									m_timerUnZoom;
 	sf::Time									m_timerUnZoomMax;
+	sf::Time									m_timerBeforeChange;
+	sf::Time									m_timerBeforeChangeMax;
+	sf::Time									m_timerChangeLookAt;
+	sf::Time									m_timerChangeLookAtMax;
 };
 
 #endif
--- Game/CharacterOcto.cpp
+++ Game/CharacterOcto.cpp
@@ -671,6 +671,11 @@ void	CharacterOcto::update(sf::Time frameTime)
 		robot->update(frameTime);
 		robot->setPosition(m_box->getPosition() + sf::Vector2f(20.f, 0.f));
 	}
+	if (!m_onGround && !m_inWater && !m_onElevator)
+		m_cameraMovement->setFall(true);
+	else
+		m_cameraMovement->setFall(false);
+
 	m_cameraMovement->follow(frameTime, m_box->getPosition());
 }
 
