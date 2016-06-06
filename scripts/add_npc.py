140d3d4268f5dd645f912ac0ae948c5945abe935
fcorbel <fcorbel@student.42.fr>
2016-06-06 13:09:03 +0200

    Update script to add npc

--- scripts/add_npc.py
+++ scripts/add_npc.py
@@ -1,6 +1,7 @@
 import fileinput
 import re
 import sys
+import shutil
 
 # This script write several file to add a npc in instance
 # /!\ It doesn't create the npc class
@@ -8,11 +9,21 @@ import sys
 # Need to add text in dialogs.txt
 
 if len(sys.argv) != 3:
-  print "Use : python add_pnj_instance.py <ClassName> <SPRITENAME_OSS>"
+  print "Use : python add_npc.py <ClassName> <SPRITENAME_OSS>"
   exit();
 
 className = sys.argv[1]
 spriteName = sys.argv[2]
+
+shutil.copy2('Npcs/PeaNpc.cpp', 'Npcs/' + className + ".cpp")
+for line in fileinput.input('Npcs/' + className + '.cpp', inplace=1):
+    line = line.replace('PeaNpc', className)
+    print (line.replace('PEA_OSS', spriteName)),
+
+shutil.copy2('Npcs/PeaNpc.hpp', 'Npcs/' + className + ".hpp")
+for line in fileinput.input('Npcs/' + className + '.hpp', inplace=1):
+    line = line.replace('PeaNpc', className)
+    print (line.replace('PEA_HPP', className.upper() + '_HPP')),
 
 for line in fileinput.input('Game/CharacterOcto.cpp', inplace=1):
     if line.startswith('//Script AddNpc'):
@@ -37,11 +48,14 @@ for line in fileinput.input('Game/Game.cpp', inplace=1):
 def get_num(x):
     return int(''.join(ele for ele in x if ele.isdigit() or ele == '.'))
 
+def get_last_num(x):
+    return get_num((x.split("+"))[-1]);
+
 saveLine = ""
 for line in fileinput.input('GameObjects/AGameObject.hpp', inplace=1):
     if line.startswith('//Script AddNpc'):
-        index = get_num(saveLine) + 1
-        print "\t" + className + " = " + str(index) + "u,\n",
+        index = get_last_num(saveLine) + 1
+        print "\t" + className + "\t\t\t\t= (1 << 15) + " + str(index) + ",\n",
         print line
     else:
         saveLine = line
@@ -62,6 +76,15 @@ for line in fileinput.input('Map/GroundManager.cpp', inplace=1):
     elif line.startswith('//Script AddNpc Factory'):
         print line,
         print "\tm_npcFactory.registerCreator<" + className + ">(" + spriteName + ");"
+    elif line.startswith('//Script AddNpc Ground'):
+        print line,
+        print "\t\t\tcase GameObjectType::" + className + ":\n",
+        print "\t\t\t\t{\n",
+        print "\t\t\t\t\t" + className + " * npc = new " + className + "();\n",
+        print "\t\t\t\t\tnpc->onFloor();\n",
+        print "\t\t\t\t\tm_npcsOnFloor.emplace_back(gameObject.first, 1, npc);\n",
+        print "\t\t\t\t}\n",
+        print "\t\t\t\tbreak;"
     else:
         print line,
 
