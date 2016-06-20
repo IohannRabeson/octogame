e32f6cf579b34e6b54a88717339bff527cdac0c3
fcorbel <fcorbel@student.42.fr>
2016-06-20 17:36:43 +0200

    Improve rain sound

--- Decors/SkyCycle.cpp
+++ Decors/SkyCycle.cpp
@@ -263,7 +263,7 @@ void SkyCycle::update(sf::Time frameTime, ABiome & biome)
 	if (biome.canCreateRain() || biome.canCreateSnow())
 		computeDrop(frameTime, biome);
 	if (m_rainSound && biome.canCreateRain() && !biome.canCreateSnow() && biome.getId() != Level::WaterA)
-		m_rainSound->setVolume(m_weather / m_dropTimerMax.asSeconds() * 10);
+		m_rainSound->setVolume(m_weather / m_dropTimerMax.asSeconds() * octo::Application::getAudioManager().getSoundVolume() * 0.5f);
 	if ((m_weather || m_thunderTimer != sf::Time::Zero) && biome.canCreateThunder())
 	{
 		computeThunder(frameTime, biome);
