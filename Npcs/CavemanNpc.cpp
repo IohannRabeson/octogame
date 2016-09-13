9b10179ed0d3062874e796fe09c6e32695fa5ece
jbalestr42 <julien.balestrieri@gmail.com>
2016-09-13 13:30:04 +0200

    fix animation caveman

--- Npcs/CavemanNpc.cpp
+++ Npcs/CavemanNpc.cpp
@@ -18,6 +18,10 @@ void CavemanNpc::setup(void)
 			Frame(sf::seconds(0.4f), {1u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {2u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {3u, sf::FloatRect(), sf::Vector2f()}),
+			});
+	getIdleAnimation().setLoop(octo::LoopMode::Loop);
+
+	getSpecial1Animation().setFrames({
 			Frame(sf::seconds(0.4f), {4u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {5u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {6u, sf::FloatRect(), sf::Vector2f()}),
@@ -26,10 +30,6 @@ void CavemanNpc::setup(void)
 			Frame(sf::seconds(0.4f), {9u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {10u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {11u, sf::FloatRect(), sf::Vector2f()}),
-			});
-	getIdleAnimation().setLoop(octo::LoopMode::Loop);
-
-	getSpecial1Animation().setFrames({
 			Frame(sf::seconds(0.4f), {12u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {13u, sf::FloatRect(), sf::Vector2f()}),
 			Frame(sf::seconds(0.4f), {14u, sf::FloatRect(), sf::Vector2f()}),
