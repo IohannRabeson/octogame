d2f524ee7dc64fd2c1047dcacf4e7b1eb2b811f3
fcorbel <fcorbel@student.42.fr>
2016-09-24 20:41:30 +0200

    Portal random close

--- GameObjects/Portal.cpp
+++ GameObjects/Portal.cpp
@@ -63,18 +63,11 @@ Portal::Portal(Level destination, ResourceKey key, ResourceKey shader, sf::Color
 		m_animationClosed.setLoop(octo::LoopMode::NoLoop);
 	
 		m_animationOpening.setFrames({
-			Frame(sf::seconds(0.4f), {0u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {1u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {2u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {3u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {4u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {5u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {6u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {7u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {8u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {9u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {10u, sf::FloatRect(), sf::Vector2f()}),
-			Frame(sf::seconds(0.4f), {11u, sf::FloatRect(), sf::Vector2f()}),
+			Frame(sf::seconds(0.4f), {16u, sf::FloatRect(), sf::Vector2f()}),
+			Frame(sf::seconds(0.4f), {15u, sf::FloatRect(), sf::Vector2f()}),
+			Frame(sf::seconds(0.4f), {14u, sf::FloatRect(), sf::Vector2f()}),
+			Frame(sf::seconds(0.4f), {13u, sf::FloatRect(), sf::Vector2f()}),
+			Frame(sf::seconds(0.4f), {12u, sf::FloatRect(), sf::Vector2f()}),
 		});
 		m_animationOpening.setLoop(octo::LoopMode::NoLoop);
 
