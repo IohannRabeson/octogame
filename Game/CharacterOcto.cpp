fdd61127fc9a542dbcd9b8ec5b2cc6394d20e57f
fcorbel <fcorbel@student.42.fr>
2016-10-07 20:42:11 +0200

    Fix octo

--- Game/CharacterOcto.cpp
+++ Game/CharacterOcto.cpp
@@ -1441,15 +1441,18 @@ void	CharacterOcto::collisionElevatorUpdate()
 
 void	CharacterOcto::kill()
 {
-	if (m_sprite.getCurrentEvent() != Death && !Progress::getInstance().isMenu())
+	if (!Progress::getInstance().isMenu())
 	{
-		Progress & progress = Progress::getInstance();
+		if (m_sprite.getCurrentEvent() != Death)
+		{
+			Progress & progress = Progress::getInstance();
 
-		m_sprite.setNextEvent(Death);
-		m_helmetParticle.canEmit(true);
-		m_helmetParticle.setPosition(getPosition() + sf::Vector2f(0.f, -25.f));
-		progress.registerDeath(getPosition());
-		progress.setKillOcto(false);
+			m_sprite.setNextEvent(Death);
+			m_helmetParticle.canEmit(true);
+			m_helmetParticle.setPosition(getPosition() + sf::Vector2f(0.f, -25.f));
+			progress.registerDeath(getPosition());
+			progress.setKillOcto(false);
+		}
 	}
 	else
 		m_sprite.setNextEvent(Idle);
